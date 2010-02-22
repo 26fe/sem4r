33c338671e1a8ed6dc27adb9adda049d4767510c
sem4r <sem4ruby@gmail.com>
2010-02-22 22:37:34 +0100

    added command line interface

---
+++ lib/sem4r/cli/cli_simple_commands.rb
@@ -0,0 +1,62 @@
+# -------------------------------------------------------------------------
+# Copyright (c) 2009-2010 Sem4r sem4ruby@gmail.com
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+# -------------------------------------------------------------------------
+
+module Sem4r
+
+  def self.simple_cli_command(command_name, description_str, &block)
+
+    cls = Class.new do
+      def initialize(account)
+        @account = account
+      end
+
+      def command_opt_parser(options)
+        opt_parser = OptionParser.new
+        opt_parser.banner= "#{self.class.description}"
+        opt_parser.on("-h", "--help", "show this message") do
+          puts opt_parser
+          options.exit = true
+        end
+      end
+
+      define_method("run") do |argv|
+        options = OpenStruct.new
+        command_opt_parser(options).parse( argv )
+        return false if options.exit
+        block.call(@account)
+      end
+    end
+
+    s = class << cls; self; end
+    s.class_eval do
+      define_method("command") { command_name }
+      define_method("description") { description_str }
+    end
+    cls
+  end
+
+  CliListClient = simple_cli_command("clients", "list clients account") { |account | account.p_client_accounts }
+  CliListReport = simple_cli_command("reports", "list reports") { |account | account.p_reports }
+
+end
