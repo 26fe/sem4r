631f87c0c2aa6cce39dddfb8551e2cac52267511
sem4r <sem4ruby@gmail.com>
2010-02-22 22:41:58 +0100

    added rspec matcher for testing xml equivalence

--- spec/sem4r/cli/cli_spec.rb
+++ spec/sem4r/cli/cli_spec.rb
@@ -1,6 +1,6 @@
 # -------------------------------------------------------------------------
 # Copyright (c) 2009-2010 Sem4r sem4ruby@gmail.com
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
@@ -8,10 +8,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -19,40 +19,23 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-# 
+#
 # -------------------------------------------------------------------------
 
-require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require File.dirname(__FILE__) + '/../../spec_helper'
 
 
-describe AdGroupAdOperation do
-  include Sem4rSpecHelper
-  
-  before(:each) do
-    @adgroup = mock("adgroup").as_null_object
-    @ad_operation = AdGroupAdOperation.new
-  end
+describe "cli" do
 
-  it "should raise missing operand" do
-    @ad_operation.operator "ADD"
-    @ad_operation.operator.should == "ADD"
-    lambda {
-      @ad_operation.to_xml(nil)
-    }.should raise_error(Sem4rError, "Missing Operand")
-  end
+  it "should divide command line arguments into two sets" do
+pending "Test"
+    args = %w{ -a -b -cc command -d --ee -f}
 
-  it "should produce xml" do
-    @adgroup.should_receive(:id).and_return(3060284754)
-    text_ad = AdGroupTextAd.new(@adgroup)
-    text_ad.headline     = "Cruise to Mars Sector 1"
-    text_ad.description1 = "Visit the Red Planet in style."
-    text_ad.description2 = "Low-gravity fun for everyone!"
-    text_ad.display_url  = "www.example.com"
-    text_ad.url          = "http://www.example.com"
-    @ad_operation.add text_ad
+    args1, command, args2 = split_args(args)
+    args1.should == %w{-a -b -cc}
+    command.should == "command"
+    args2.should == %w{-d --ee -f}
 
-    expected_xml = read_model("//operations", "services", "bulk_mutate_job_service", "mutate-req.xml")
-    @ad_operation.to_xml('operations').should xml_equivalent(expected_xml)
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,53 +6,9 @@ include Sem4r
 
 require 'sem4r_spec_helper'
 
-
-
 Spec::Runner.configure do |config|
 #  config.use_transactional_fixtures = true
 #  config.use_instantiated_fixtures  = false
 #  config.fixture_path = RAILS_ROOT + '/spec/fixtures'
 end
-
-
-###
-## rSpec Hash additions.
-##
-## From
-##   * http://wincent.com/knowledge-base/Fixtures_considered_harmful%3F
-##   * Neil Rahilly
-#
-#class Hash
-#
-#  ##
-#  # Filter keys out of a Hash.
-#  #
-#  #   { :a => 1, :b => 2, :c => 3 }.except(:a)
-#  #   => { :b => 2, :c => 3 }
-#
-#  def except(*keys)
-#    self.reject { |k,v| keys.include?(k || k.to_sym) }
-#  end
-#
-#  ##
-#  # Override some keys.
-#  #
-#  #   { :a => 1, :b => 2, :c => 3 }.with(:a => 4)
-#  #   => { :a => 4, :b => 2, :c => 3 }
-#
-#  def with(overrides = {})
-#    self.merge overrides
-#  end
-#
-#  ##
-#  # Returns a Hash with only the pairs identified by +keys+.
-#  #
-#  #   { :a => 1, :b => 2, :c => 3 }.only(:a)
-#  #   => { :a => 1 }
-#
-#  def only(*keys)
-#    self.reject { |k,v| !keys.include?(k || k.to_sym) }
-#  end
-#
-#end
 
