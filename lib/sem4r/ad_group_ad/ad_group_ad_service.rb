98709d4c405c3bdfcebc906532d0191fb7f58231
sem4r <sem4ruby@gmail.com>
2010-04-04 19:59:48 +0200

    grouping creations of AdGroupAd

--- lib/sem4r/ad_group_ad/ad_group_ad_service.rb
+++ lib/sem4r/ad_group_ad/ad_group_ad_service.rb
@@ -34,7 +34,7 @@ module Sem4r
     end
 
     soap_call_v2009 :all,   :mutate => false
-    soap_call_v2009 :create
+    soap_call_v2009 :mutate
 
     private
 
@@ -48,15 +48,8 @@ module Sem4r
       EOFS
     end
 
-    def _create(xml)
-      <<-EOFS
-      <mutate xmlns="#{@service_namespace}">
-        <operations xsi:type="AdGroupAdOperation">
-          <operator>ADD</operator>
-            #{xml}
-        </operations>
-      </mutate>
-      EOFS
+    def _mutate(xml)
+      "<mutate xmlns=\"#{@service_namespace}\">#{xml}</mutate>"
     end
 
   end
